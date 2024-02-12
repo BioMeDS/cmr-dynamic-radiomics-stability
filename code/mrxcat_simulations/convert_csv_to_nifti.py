4a768d5b15ace757beefc226fd73985e28e3fd63
Mike Klaus <105484601+wagon-master@users.noreply.github.com>
2024-02-12 14:14:41 +0100

    Remove comment

--- code/mrxcat_simulations/convert_csv_to_nifti.py
+++ code/mrxcat_simulations/convert_csv_to_nifti.py
@@ -6,10 +6,9 @@ import nibabel as nib
 
 
 def convert_csv_to_nifti(csv_file, nifti_file):
+
     my_data = genfromtxt(f"{csv_file}",  delimiter=',')
     my_data = np.expand_dims(my_data, axis=2)
     nib.save(nib.Nifti1Image(my_data, np.eye(4)), f"{nifti_file}")
 
-# for i in range(0, len(snakemake.input)):
-#     convert_csv_to_nifti(snakemake.input[i], snakemake.output[i])
 convert_csv_to_nifti(snakemake.input[0], snakemake.output[0])
