d48a1385e05e0c8433fa958738e3689770471191
Mike Klaus <105484601+wagon-master@users.noreply.github.com>
2024-03-11 16:00:04 +0100

    Add working code for feature normalization and later needed snakemake env file

---
+++ code/mrxcat_simulations/feature_normalization.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+
+normalize_feature_df <- function(df_location, save_location, standard_df) {
+  files <- dir(df_location)
+  names(files) <- files
+  data <- files %>%
+    map_df(
+      ~read_csv(paste0(str_glue("{df_location}/"), .x), show_col_types = FALSE),
+      .id = "file"
+    ) %>%
+    pivot_longer(
+      names_to = "feature",
+      values_to = "value",
+      `original_shape_Elongation`:`wavelet-LLL_ngtdm_Strength`
+    )
+  data_left_join <- data %>%
+    filter(file == standard_df) %>%
+    group_by(feature) %>%
+    summarize(mean_value = mean(value), sd_value = sd(value))
+  data_long <- left_join(data, data_left_join, by = c("feature")) %>%
+    mutate(value = (value - mean_value) / sd_value, .keep = "unused") %>%
+    pivot_wider(names_from = feature, values_from = value)
+  files <- data_long %>% distinct(file)
+  data_long %>%
+    write_csv(file.path(save_location))
+}
+
+normalize_feature_df(
+  snakemake@input[[1]],
+  snakemake@output[[1]],
+  snakemake@params
+)
