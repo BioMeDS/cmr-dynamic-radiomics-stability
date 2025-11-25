7c2a9ebb6b03c22749e88148ca96d8823b8278ec
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-11-25 09:50:48 +0100

    Add supplementary psnr calculation script

---
+++ code/supplement/psnr.py
@@ -0,0 +1,13 @@
+import skimage as ski
+import nibabel as nib
+import sys
+
+i1 = sys.argv[1]
+i2 = sys.argv[2]
+
+im1 = nib.load(i1).get_fdata()
+im2 = nib.load(i2).get_fdata()
+
+psnr = ski.metrics.peak_signal_noise_ratio(im1, im2, data_range=im1.max())
+
+print(f"{i1}\t{i2}\t{psnr}")
