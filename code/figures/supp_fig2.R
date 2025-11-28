2318b23a351c3a8376637e46fa3d7f03964d63b5
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-11-28 12:25:39 +0100

    Fix axis labels

--- code/figures/supp_fig2.R
+++ code/figures/supp_fig2.R
@@ -4,13 +4,13 @@ theme_set(theme_light())
 summarized <- read_tsv("figures/supp_tab1.tsv")
 
 supp_fig2 <- summarized |>
-  ggplot(aes(x=mean_mae, y=mean_within_mae)) +
-    geom_point() +
-    scale_x_continuous(limits=c(0,1)) +
-    scale_y_continuous(limits=c(0,1)) +
-    geom_hline(yintercept=.4) +
-    geom_vline(xintercept = .4) +
-    xlab("Mean average error (total)") +
-    ylab("Mean average error (within noise class)")
+  ggplot(aes(x = mean_mae, y = mean_within_mae)) +
+  geom_point() +
+  scale_x_continuous(limits = c(0, 1)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = .4) +
+  geom_vline(xintercept = .4) +
+  xlab("Mean mae (total)") +
+  ylab("Mean mae (within noise class)")
 
-ggsave("figures/supp_fig2.svg", plot=supp_fig2, width=10, height=8)
+ggsave("figures/supp_fig2.svg", plot = supp_fig2, width = 10, height = 8)
