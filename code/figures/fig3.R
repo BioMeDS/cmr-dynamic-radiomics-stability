a733e2d231e98e55467199d1105fafde2570d09b
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-12-03 20:58:45 +0100

    Update all figures
    recreate png and eps with
    for i in figures/*.svg
    do
        inkscape -o ${i/svg/png} $i
        inkscape -o ${i/svg/eps} $i
    done

--- code/figures/fig3.R
+++ code/figures/fig3.R
@@ -4,32 +4,37 @@ theme_set(theme_minimal())
 combined <- read_tsv("analysis/combined/features.tsv")
 ranks_combined <- read_tsv("analysis/combined/ranks.tsv")
 
-make_comparison_plot <- function(feature_name, ylabel="{feature_name}"){
+make_comparison_plot <- function(feature_name, ylabel = "{feature_name}") {
   combined %>%
-    left_join(filter(ranks_combined, Feature_name==.env$feature_name), by="patient") %>%
-    mutate(patient = glue::glue("{patient} ({rank})")) %>%
-    ggplot(aes(x=ID, y=.data[[feature_name]], group=interaction(noise, seed))) +
-      geom_line(alpha=.2) +
-      geom_line(data = ~filter(., parse_number(noise)==0 | (noise=="snr50")), color="red") +
-      facet_wrap(~patient, scales="free_x") +
-      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-      xlab("frame") +
-      ylab(str_glue(ylabel))
+    left_join(filter(ranks_combined, Feature_name == .env$feature_name), by = "patient") %>%
+    mutate(
+      patient = str_replace(patient, "patient", "ACDC"),
+      patient = str_replace(patient, "Proband X", "BAE"),
+      patient = str_replace(patient, "sim", "MRXCAT"),
+      patient = glue::glue("{patient} ({rank})")
+    ) %>%
+    ggplot(aes(x = ID, y = .data[[feature_name]], group = interaction(noise, seed))) +
+    geom_line(alpha = .2) +
+    geom_line(data = ~ filter(., parse_number(noise) == 0 | (noise == "snr50")), color = "red") +
+    facet_wrap(~patient, scales = "free_x") +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    xlab("frame") +
+    ylab(str_glue(ylabel))
 }
 
-fig3 <- make_comparison_plot("wavelet-LLL_glcm_Idn", ylabel="normalized feature value ({feature_name})")
-ggsave("figures/fig3.svg", plot=fig3, width=10, height=10)
+fig3 <- make_comparison_plot("wavelet-LLL_glcm_Idn", ylabel = "normalized feature value ({feature_name})")
+ggsave("figures/fig3.svg", plot = fig3, width = 12, height = 12)
 
 rank_order_by_median <- ranks_combined %>%
   filter(!is.na(rank)) %>%
   group_by(Feature_name) %>%
-  summarize(median_rank=median(rank), mad_rank=mad(rank)) %>%
+  summarize(median_rank = median(rank), mad_rank = mad(rank)) %>%
   mutate(Feature_name = fct_reorder(Feature_name, median_rank)) %>%
   pull(Feature_name) %>%
   levels()
 
-pdf("figures/fig3_supplement.pdf", onefile = TRUE)
-for (i in rank_order_by_median) {
-  print(make_comparison_plot(i))
-}
-dev.off()
+# pdf("figures/fig3_supplement.pdf", onefile = TRUE)
+# for (i in rank_order_by_median) {
+#  print(make_comparison_plot(i))
+# }
+# dev.off()
